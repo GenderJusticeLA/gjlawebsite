03ebc5443e8b73aff52d73aaf98f32732ef203df
Hannah Howard <hannah@Macintosh.local>
2012-09-16 19:48:24 -0700

    Generated new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gjlawebsite::Application.config.secret_token = '8e4aef26d6bb00de12fe059eead972fd8c543ef213d921d045cc27a6f01c83f5ec73cd9c0bf33ca424ea20a296684ec75a96c48b6a79549a65ae45a9aa6e4c5b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Gjlawebsite::Application.config.session_store :cookie_store, key: '_gjlawebsite_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Gjlawebsite::Application.config.session_store :active_record_store
